8f96a021945b573114e4e2bfbdcaf97113d494a9
Felix Lin <felixlin77@gmail.com>
2025-02-06 21:52:05 +1100

    configured vite for GitHub pages and adjusted deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/BallDropInator/'
+export default defineConfig(({ command, mode }) => {
+  const isGitHubPages = command === 'build' && mode === 'production'
+
+  return {
+    plugins: [react()],
+    base: isGitHubPages ? '/BallDropInator/' : '/',
+    build: {
+      outDir: 'dist',
+      rollupOptions: {
+        input: 'index.html',
+        output: {
+          format: 'iife'
+        }
+      }
+    }
+  }
 })
